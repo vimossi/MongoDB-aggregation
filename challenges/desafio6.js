6395670922149b62f0635252d57aaa42baa3f50f
vimossi <vini_agg@hotmail.com>
2021-12-14 12:24:33 -0400

    verifica req 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,25 @@
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won \d+ Oscar/ },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      maior_rating: true,
+      menor_rating: true,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
