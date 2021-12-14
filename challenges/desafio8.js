3882f0f7d8976bd92d633c08a9570595b24ef83b
vimossi <vini_agg@hotmail.com>
2021-12-14 15:22:43 -0400

    resolve req 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,2 +1,32 @@
-db.air_alliances.aggregate([
+db.air_routes.aggregate([
+  {
+    $match: {
+      airplane: { $in: ["747", "380"] },
+    },
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      localField: "airline.name",
+      foreignField: "airlines",
+      as: "alliance_routes",
+    },
+  },
+  {
+    $unwind: "$alliance_routes",
+  },
+  {
+    $group: {
+      _id: "$alliance_routes.name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
 ]);
